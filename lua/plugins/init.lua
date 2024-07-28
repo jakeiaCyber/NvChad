ba27e3682723c2b14fefce325681fcd98f9f15eb
jakeia <azbazb011@gmail.com>
2024-07-28 10:20:39 +0800

    upodate

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,14 +1,5 @@
 return {
   {
-    "andreadev-it/shade.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("shade").setup {
-        exclude_filetypes = { "NvimTree" },
-      }
-    end,
-  },
-  {
     "jvgrootveld/telescope-zoxide",
     lazy = true,
     specs = {
